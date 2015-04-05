5354572e5bd1f224f44430624265b2d24ff4a083
priyankakumbha <pramodkumbhar789@gmail.com>
2015-04-06 00:09:11 +1000

    added hot cold logic and display

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 
 $(document).ready(function(){
+	var randomNumber =  Math.floor((Math.random() * 100) + 1);
+	console.log("random number generated" +randomNumber);
+	/*$(document).on("click", "#guessButton", function(){*/
+		$("form").submit(function(event){
+		
+		event.preventDefault();
+		var guessNumber= $("#userGuess").val();
+		console.log("guessnumber" +guessNumber);
+		var guessDifference = randomNumber-guessNumber;
+   var hotcoldResult= checkTemperature(guessDifference);
+   console.log(hotcoldResult );
+   $( "#feedback" ).text(hotcoldResult);
+ 
+    
+});
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -13,5 +28,42 @@ $(document).ready(function(){
   	});
 
 });
+function checkTemperature(guessDifference){
+   var resultstring='' ;
+   if(guessDifference==0){
+     
+     
+      resultstring="you got it";
+   } else if(guessDifference>0 &&guessDifference<=10){
+   
+   	resultstring="you are very hot";
+      }
+   else if(guessDifference>=10 && guessDifference<=20){
+   	
+   	resultstring="you are hot";
+      }
+      else if(guessDifference>=20 && guessDifference<=30){
+   	
+   	resultstring="you are warm";
+      }
+     else if(guessDifference>=30 && guessDifference<=50){
+   	
+   	resultstring="you are cold";
+      }
+
+   else if(guessDifference>=50) {
+   	
+   	resultstring="you are ice cold";
+      
+   }
+   else{
+   	
+   	resultstring="you are freezing";
+   }
+  
+   return resultstring;
+    
+  }
+
 
 
