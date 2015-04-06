5331e8b7f1a99fec2df95520258a009d4c49f91e
priyankakumbha <pramodkumbhar789@gmail.com>
2015-04-06 19:52:24 +1000

    added logic for new game, and added validation for number

--- js/app.js
+++ js/app.js
@@ -1,19 +1,35 @@
 
 $(document).ready(function(){
-	var randomNumber =  Math.floor((Math.random() * 100) + 1);
-	console.log("random number generated" +randomNumber);
+	var randomNumber=generateRandomNumber();
+  var guessCounter = 0;
+  console.log("random number generated" +randomNumber);
+  $(document).on("click", ".new", function(){
+   startNewGame();
+   guessCounter= 0;
+   randomNumber=generateRandomNumber();
+   console.log("random number generated new game" +randomNumber);
+
+  });
+
+
 	/*$(document).on("click", "#guessButton", function(){*/
 		$("form").submit(function(event){
-		
-		event.preventDefault();
-		var guessNumber= $("#userGuess").val();
+	  event.preventDefault();
+		var guessNumberString= $("#userGuess").val();
+    var guessNumber= +guessNumberString;
+    if(isNaN(guessNumber)) {
+    $( "#feedback" ).text("please enter a valid number");
+    } else{
 		console.log("guessnumber" +guessNumber);
+    console.log("randomNumber *" +randomNumber);
 		var guessDifference = randomNumber-guessNumber;
-   var hotcoldResult= checkTemperature(guessDifference);
-   console.log(hotcoldResult );
-   $( "#feedback" ).text(hotcoldResult);
- 
-    
+    var hotcoldResult= checkTemperature(guessDifference);
+    guessCounter= guessCounter + 1;
+    console.log(hotcoldResult );
+    setFeedback(hotcoldResult);
+    setGuessCounter(guessCounter);
+    setGuessList(guessNumber);
+    } 
 });
 	
 	/*--- Display information modal box ---*/
@@ -28,6 +44,7 @@ $(document).ready(function(){
   	});
 
 });
+/* ---- Calculatioan of feedback ----*/
 function checkTemperature(guessDifference){
    var resultstring='' ;
    if(guessDifference==0){
@@ -37,33 +54,49 @@ function checkTemperature(guessDifference){
    } else if(guessDifference>0 &&guessDifference<=10){
    
    	resultstring="you are very hot";
-      }
-   else if(guessDifference>=10 && guessDifference<=20){
+    }else if(guessDifference>=10 && guessDifference<=20){
    	
    	resultstring="you are hot";
-      }
-      else if(guessDifference>=20 && guessDifference<=30){
+    }else if(guessDifference>=20 && guessDifference<=30){
    	
    	resultstring="you are warm";
-      }
-     else if(guessDifference>=30 && guessDifference<=50){
+    }else if(guessDifference>=30 && guessDifference<=50){
    	
    	resultstring="you are cold";
-      }
-
-   else if(guessDifference>=50) {
-   	
+   }else if(guessDifference>=50) {
    	resultstring="you are ice cold";
-      
    }
    else{
-   	
    	resultstring="you are freezing";
    }
-  
    return resultstring;
-    
-  }
+}
 
+function generateRandomNumber() {
+  var randomNumber =  Math.floor((Math.random() * 100) + 1);
+  return randomNumber;
+}
+
+function setFeedback(feedback) {
+$( "#feedback" ).text(feedback);
+}
+
+function setGuessCounter(guessCount) {
+$("#count").text(""+guessCount);
+
+}
+function setGuessList(guessNumber) {
+$( "#guessList" ).append(' '+guessNumber);
+
+}
+function startNewGame() {
+   console.log("starting new game");
+   $( "#guessList" ).empty();
+    
+    setGuessCounter(0);
+    setFeedback("Make your Guess");
+    $("#userGuess").val('');
+  
+  }
 
 
